7fdf958e7f67e3503edf4d4d43d1aa15209958c7
Lane Kolbly <lane@rscheme.org>
2019-01-01 12:14:10 -0600

    Updated to crash when encountering a site with multiple fields, fixed URL parser, check for use dmy or use mdy date tags and adjust accessed date and asof date accordingly, add check to verify that alexa data being replaced is in a known sanctioned set, download a page whitelist from OKBot's whitelist.

--- alexa.py
+++ alexa.py
@@ -49,9 +49,14 @@ def get_alexa(url):
 		pprint.pprint(xml)
 
 def get_field(infobox, fieldname):
+	res = None
 	for field in infobox[1]:
 		if field.startswith(fieldname+"="):
-			return field
+			#return field
+			if res != None:
+				raise Exception("Site has two {} fields".format(fieldname))
+			res = field
+	return res
 
 def parse_url(url):
 	'''
@@ -61,8 +66,8 @@ def parse_url(url):
 	[http://<domain>/ <domain>]
 	'''
 	url = url.lower()
-	if url.startswith("{{url"):
-		url = url.split("|")[1].strip("{}")
+	if url.startswith("{{url") or url.startswith("{{URL"):
+		url = url.split("|")[1].split("}}")[0]
 		if url.startswith("http://") or url.startswith("https://"):
 			url = url.split("//")[1]
 			url = url.split("/")[0]
@@ -73,8 +78,19 @@ def parse_url(url):
 		return url
 	pass
 
-def update_page(page):
+def update_page(page, whitelisted=False):
 	templates = page.templatesWithParams()
+	# Find use dmy or use mdy
+	use_dmy = False
+	use_mdy = False
+	for template in templates:
+		if template[0].title() == "Template:Use dmy dates":
+			use_dmy = True
+			break
+		if template[0].title() == "Template:Use mdy dates":
+			use_mdy = True
+			break
+
 	for template in templates:
 		if template[0].title() == "Template:Infobox website":
 			url = get_field(template, "url")
@@ -115,16 +131,54 @@ def update_page(page):
 			y = now.year
 			m = now.month
 			d = now.day
-			new_alexa = """{{{{{}}}}} {:,} ({{{{as of|{}|{}|{}}}}})<ref name="alexa">{{{{cite web|url= http://www.alexa.com/siteinfo/{} | publisher= [[Alexa Internet]] |title={} |accessdate= {}-{}-{} }}}}</ref> <!-- Updated monthly by LkolblyBot --> """.format(signal, int(rank), y, m, d, domain, sitetitle, y, m, d)
+			accessed_date = "{}-{}-{}".format(y, m, d)
+			MONTHS = [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"
+			]
+			if use_dmy:
+				accessed_date = "{} {} {}".format(d, MONTHS[m-1], y)
+			if use_mdy:
+				accessed_date = "{} {}, {}".format(MONTHS[m-1], d, y)
+			asof_tag = "{{{{as of|{}|{}|{}}}}}".format(y, m, d)
+			if use_mdy:
+				asof_tag = "{{{{as of|{}|{}|{}|df=US}}}}".format(y, MONTHS[m-1], d)
+			new_alexa = """{{{{{}}}}} {:,} ({})<ref name="alexa">{{{{cite web|url= http://www.alexa.com/siteinfo/{} | publisher= [[Alexa Internet]] |title={} |accessdate= {} }}}}</ref> <!-- Updated monthly by LkolblyBot --> """.format(signal, int(rank), asof_tag, domain, sitetitle, accessed_date)
 
 			print(domain)
 			current_alexa = "=".join(alexa.split("=")[1:])
+
+			# We will only replace the current alexa if it's in a known set of formats
+			if len(current_alexa.strip(" \r\n")) == 0:
+				continue
+			replaceable_regexes = [
+				r'.*Updated monthly by LkolblyBot.*', # The LKolblyBot comment tag :D
+				r'{{\w+}} [0-9,]+ \(\w+; [0-9/]+\)', # e.g. Ethnologue
+				r'{{\w+}} [0-9,]+ \({{as of[0-9|]+}}\)<ref\s+(name="alexa")?>{{.*}}</ref>', # The OKBot format
+				r'{{\w+}} [0-9,]+ \({{as of[0-9|]+\|alt=[0-9a-zA-Z ]+}}\)<ref\s+(name="alexa")?>{{.*}}</ref>', # The OKBot format w/ alt date
+			]
+			matches = [re.match(r, current_alexa) for r in replaceable_regexes]
+			is_disallowed = all(r == None for r in matches)
+			if is_disallowed and not whitelisted:
+				print("Page didn't match regex with {}".format(current_alexa))
+				continue
+
 			print("Current alexa: {}".format(current_alexa))
 			print("New alexa:     {}".format(new_alexa))
 			#newtext = page.text.replace(current_alexa, new_alexa)
 			newtext = re.sub(r"\|\s*alexa\s*=\s*{}".format(re.escape(current_alexa)), lambda m: "| alexa = {}".format(new_alexa), page.text)
-			import sys
-			sys.stdout.writelines(difflib.context_diff(list(map(lambda s: s+"\n", page.text.split("\n"))), list(map(lambda s: s+"\n", newtext.split("\n")))))
+			#import sys
+			#sys.stdout.writelines(difflib.context_diff(list(map(lambda s: s+"\n", page.text.split("\n"))), list(map(lambda s: s+"\n", newtext.split("\n")))))
 			print()
 			print()
 
@@ -132,7 +186,16 @@ def update_page(page):
 			#page.save("Update Alexa ranking")
 
 if __name__ == "__main__":
+	# @TODO: Move this to a LkolblyBot page
+	page = pywikibot.Page(site, "User:OsamaK/AlexaBot.js")
+	whitelist = [l.split(" ")[0].strip('"') for l in page.text.split("\n")]
+
+	for pagetitle in whitelist:
+		update_page(pywikibot.Page(site, pagetitle), True)
+
 	for pagetitle in open("../potential_alexas").readlines():
+		if pagetitle in whitelist:
+			continue
 		update_page(pywikibot.Page(site, pagetitle))
 	#print(get_alexa("darwinawards.com"))
 
@@ -140,6 +203,7 @@ if __name__ == "__main__":
 	#	"{{url|http://www.darwinawards.com/}}",
 	#	"{{URL|ethnologue.com}}",
 	#	"{{URL|www.google.com|Google.com}}",
-	#	"[http://foldoc.org/ foldoc.org]"
+	#	"[http://foldoc.org/ foldoc.org]",
+	#	"{{URL|https://www.4chan.org}}<br />{{URL|https://www.4channel.org}}"
 	#]:
 	#	print(parse_url(url))
